1bf30079d7c54b265fd5ddb026a32a8964488384
James Prentice <jamesprenticez@gmail.com>
2021-05-01 15:27:00 +1200

    Lesson 2.3 - Setting up Routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import {readdirSync} from "fs"
 const morgan = require("morgan")
 require("dotenv").config()
 
@@ -11,10 +12,10 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 
-//Route
-app.get('/', (req, res) => {
-    res.send('you hit endpoint')
-})
+//Auto Load Routes with map and pre-fix '/api' 
+readdirSync("./routes").map((r) =>
+    app.use("/api", require(`./routes/${r}`))
+)
 
 //Port
 const port = process.env.PORT || 8000
