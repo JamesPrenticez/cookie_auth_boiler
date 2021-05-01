b1627d8e33fd78ff8e7f65eab2f3708764835012
James Prentice <jamesprenticez@gmail.com>
2021-05-01 14:55:47 +1200

    Lesson 2.2 - Express Setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log('server setup')
+import express from 'express'
+import cors from 'cors'
+const morgan = require("morgan")
+require("dotenv").config()
+
+//Create Express App
+const app = express()
+
+//Apply Middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+//Route
+app.get('/', (req, res) => {
+    res.send('you hit endpoint')
+})
+
+//Port
+const port = process.env.PORT || 8000
+
+//Listen to our running server
+app.listen(port, () => console.log(`Server is running on port ${port}`))
